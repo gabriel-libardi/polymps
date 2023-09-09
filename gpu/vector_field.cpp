d437be5ee1b01a293ff0aff4362f4f06906ac5e6
gabriel-libardi <libardi.120205@protonmail.com>
2023-09-09 16:38:38 -0300

    Boostrap for basic operator code

---
+++ gpu/vector_field.cpp
@@ -0,0 +1,7 @@
+#include "vector_field.hpp"
+#include "opencl.hpp"
+#include <cstdio>
+
+int main() {
+    printf("Olá");
+}
---
+++ gpu/vector_field.hpp
@@ -0,0 +1,5 @@
+#ifndef __VECTOR_FIELD_HPP__
+#define __VECTOR_FIELD_HPP__
+
+
+#endif
